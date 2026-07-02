044b4c6577c197b93efd382b62b5281c48463d90
Salman <salmanalthof31@gmail.com>
2026-07-02 23:39:32 +0700

    feat: liast all fetch api backend

---
+++ src/services/imageApi.ts
@@ -0,0 +1,38 @@
+"use server"
+
+import { ResponseStatus } from "@/types/interfacesImageApi";
+
+const API_SMARANCH = process.env.API_BE;
+
+export async function fetchUploadImage(file: FormData): Promise<ResponseStatus> {
+    try {
+        const response: Response = await fetch(`${API_SMARANCH}/image/upload`, {
+            method: "POST",
+            body: file 
+        }) 
+        if(!response.ok) throw new Error(`${response.statusText}! status: ${response.status}`);
+        return response.json()
+    } catch (error: unknown) {
+        throw error
+    }
+}
+
+export async function fetchStatusImageProcess(jobId: string): Promise<ResponseStatus> {
+    try {
+        const response: Response = await fetch(`${API_SMARANCH}/image/${jobId}`) 
+        if(!response.ok) throw new Error(`${response.statusText}! status: ${response.status}`);
+        return response.json()
+    } catch (error: unknown) {
+        throw error
+    }
+}
+
+export async function donwloadImage(jobId: string): Promise<Blob> {
+    try {
+        const response: Response = await fetch(`${API_SMARANCH}/image/${jobId}/download`);
+        if(!response.ok) throw new Error(`${response.statusText}! status: ${response.status}`);
+        return response.blob();
+    } catch (error: unknown) {
+        throw error
+    }
+}
